106343f83649f398dfd29698396d46fcbc9bdbb0
Penis Joe <hajimemashitekaczynski@gmail.com>
2025-05-04 20:28:46 +0200

    fixed link error

--- src/routes/security/+page.svelte
+++ src/routes/security/+page.svelte
@@ -1,6 +1,6 @@
 
 <script>
-    import { base } from '$app/paths';
+    import { base } from '$app/paths'; 
 </script><main>
     <div class="main-title">
         <h1>Security + Resources</h1>
@@ -16,7 +16,7 @@
         <div class="card-body items-center text-center">
           <h2 class="card-title">Security</h2>
           <p>Little infodump about VPN's, distros and other security measures.</p>
-          <div class="card-actions"><a href="/security/vpn">
+          <div class="card-actions"><a href="{base}/security/vpn">
             <button class="btn btn-primary">Learn More</button></a>
           </div>
         </div>
@@ -32,7 +32,7 @@
         <div class="card-body items-center text-center">
           <h2 class="card-title">Torrent Clients</h2>
           <p>Lightweight, secure, and fast Torrent Clients.</p>
-          <div class="card-actions"><a href="/security/torrent">
+          <div class="card-actions"><a href="{base}/security/torrent">
             <button class="btn btn-primary">Learn More</button></a>
           </div>
         </div>
@@ -48,7 +48,7 @@
         <div class="card-body items-center text-center">
           <h2 class="card-title">Browsers</h2>
           <p>Privacy-focused browsers</p>
-          <div class="card-actions"><a href="/security/browsers">
+          <div class="card-actions"><a href="{base}/security/browsers">
             <button class="btn btn-primary">Learn More</button></a>
           </div>
         </div>
@@ -64,7 +64,7 @@
         <div class="card-body items-center text-center">
           <h2 class="card-title">Operating Systems</h2>
           <p>Linux-based operating systems with a focus on privacy, integrity, and efficiency.</p>
-          <div class="card-actions"><a href="/security/distros">
+          <div class="card-actions"><a href="{base}/security/distros">
             <button class="btn btn-primary">Learn More</button></a>
           </div>
         </div>
@@ -80,7 +80,7 @@
         <div class="card-body items-center text-center">
           <h2 class="card-title">Software</h2>
           <p>Useful software that can aid you.</p>
-          <div class="card-actions"><a href="/security/software">
+          <div class="card-actions"><a href="{base}/security/software">
             <button class="btn btn-primary">Learn More</button></a>
           </div>
         </div>
@@ -96,7 +96,7 @@
         <div class="card-body items-center text-center">
           <h2 class="card-title">File Hosts & Download Managers</h2>
           <p>Fast, free, and anonymous file hosts with quick and efficient download managers.</p>
-          <div class="card-actions"><a href="/security/fhosts">
+          <div class="card-actions"><a href="{base}/security/fhosts">
             <button class="btn btn-primary">Learn More</button></a>
           </div>
         </div>
